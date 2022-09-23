4e56d14ed7516611b13d8a8d63d6185e3cf52926
Agnes <agnes.rodhe@gmail.com>
2022-09-23 15:22:27 +0200

    eslint fixed

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
 const documents = require('./routes/documents');
-const { SocketAddress } = require("net");
 
 const app = express();
 const httpServer = require("http").createServer(app);
@@ -39,14 +38,12 @@ const io = require("socket.io")(httpServer, {
 io.sockets.on('connection', function(socket) {
     socket.on('create', function(room) {
         socket.join(room);
-
     });
     socket.on("doc", function(data) {
         socket.to(data["_id"]).emit("doc", data);
         socket.broadcast.emit("doc", data);
-    })
+    });
 });
-
 
 const server = httpServer.listen(port, () => console.log(`Example API listening on port ${port}!`));
 
